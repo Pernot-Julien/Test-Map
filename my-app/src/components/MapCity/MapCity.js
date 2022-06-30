fa112afc83374c5e62422093b539b0cb3509a768
Pernot-Julien <roulio555@gmail.com>
2022-06-30 11:40:26 +0200

    stable version with fed markers and popup

--- my-app/src/components/MapCity/MapCity.js
+++ my-app/src/components/MapCity/MapCity.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Map, {Marker} from 'react-map-gl';
+import Map, {Marker,GeolocateControl,Popup } from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -7,6 +7,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 const MAPBOX_TOKEN = 'pk.eyJ1Ijoicm91bGlvNTU1IiwiYSI6ImNsNHlsZDAwczIwcm4za21sZXlqc2hkMGEifQ.0ZWMXSk670FzzLM1gFR5kA'; // Set your mapbox token here
 
 function MapCity() {
+  const [showPopup, setShowPopup] = React.useState(true);
   return (
     <>
       <h1 className="text-3xl ml-[2%] mt-[1%]">Contacts</h1>
@@ -21,9 +22,16 @@ function MapCity() {
           mapStyle="mapbox://styles/mapbox/streets-v9"
           mapboxAccessToken={MAPBOX_TOKEN}
         >
+          {showPopup && (
+      <Popup longitude={6.024942} latitude={47.24438}
+        anchor="bottom"
+        onClose={() => setShowPopup(false)}>
+        Vous êtes ici
+      </Popup>)}
           <Marker longitude={6.024942} latitude={47.237038} color="red" />
           <Marker longitude={6.002154} latitude={47.240724} color="red" />
           <Marker longitude={6.047449} latitude={47.233789} color="red" />
+          <GeolocateControl />
         </Map>
       </div>
     </>
