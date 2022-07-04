790efc94969bad3d43983b6b64709b07c9d65267
Pernot-Julien <roulio555@gmail.com>
2022-07-04 15:30:03 +0200

    problem multi windows

--- my-app/src/components/MapCity/MapCity.js
+++ my-app/src/components/MapCity/MapCity.js
@@ -17,15 +17,25 @@ function MapCity() {
 
   const [showPopup, setShowPopup] = React.useState(true);
   const [markers, setMarkers] = React.useState([]);
-
+  const [closeWindow, setCloseWindow] = React.useState(true);
+ 
   const handleClick = (event) => {
+    setCloseWindow(closeWindow => true);
     const longitude = event.lngLat.lng;
     const latitude = event.lngLat.lat;
-    //console.log(longitude, latitude);
-    setMarkers(markers => [...markers,{long:longitude, lat:latitude}]);
+    const left = event.point.x;
+    const top = event.point.y;
+    console.log(event);
+    setMarkers(markers => [...markers,{long:longitude, lat:latitude, left:left, top:top}]);
     console.log(markers);
   };
-   
+  const handleClickButton =(event) => {
+    setCloseWindow(closeWindow => false);
+    markers.pop();
+    console.log(closeWindow);
+  };
+
+  
   return (
     <>
       <h1 className="text-3xl ml-[2%] mt-[1%]">Contacts</h1>
@@ -51,15 +61,33 @@ function MapCity() {
           <Marker longitude={6.024942} latitude={47.237038} color="red" />
           <Marker longitude={6.002154} latitude={47.240724} color="red" />
           <Marker longitude={6.047449} latitude={47.233789} color="red" />
-            {
-            markers.map((coordinate) => (
-              <>
-              <p>fsdfsdfsdf</p>
-              <Marker longitude={coordinate.long} latitude={coordinate.lat} color="red" />
+          {closeWindow &&
+            markers.map((coordinate) => (  
+              <>     
+              <Marker longitude={coordinate.long} latitude={coordinate.lat} color="red" key={coordinate.id} />
+               <div className="absolute z-10 text-sm bg-gray-200 border-2 border-black w-[220px] h-[270px]" style={{position:'absolute', left:`${coordinate.left +"px"}` ,top:`${coordinate.top+"px"}`}}>
+               <button onClick={handleClickButton}>X</button>
+               <form className='flex flex-col'>
+               <label htmlFor="Nom">Nom</label>
+                <input type="text" className='mt-[10px] w-[90%] mx-auto'/>
+                <label htmlFor="Description" className='mt-[10px]'>Description</label>
+                  <textarea id="Description" name="Description" className="mt-[10px] w-[90%] mx-auto">
+                  </textarea>
+                <select name="type" className="mt-[20px] w-[90%] mx-auto h-[25px]">
+                    <option value="">--Choisir une option--</option>
+                    <option value="Entreprise">Entreprise</option>
+                    <option value="Particulier">Particulier</option>
+                    <option value="Collectivité">Collectivité</option>
+                </select>
+                <input type="submit" value="Ajouter" className="border-1 border-black bg-blue-600 h-[30px] w-[100px] mx-auto mt-[20px]"/>
+               </form>
+              </div>
               </>
             ))
-            }  
+           }  
           <GeolocateControl />
+          <div>
+          </div>
         </Map>
       </div>
     </>
