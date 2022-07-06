945e35642b4915ccd9d5619f6becc9ecae31497d
Pernot-Julien <roulio555@gmail.com>
2022-07-06 15:23:30 +0200

    drag and drop map

--- my-app/src/components/MapCity/MapCity.js
+++ my-app/src/components/MapCity/MapCity.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Map, {Marker,GeolocateControl,Popup } from 'react-map-gl';
+import Map, {Marker} from 'react-map-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
 
@@ -8,14 +8,9 @@ const MAPBOX_TOKEN = 'pk.eyJ1Ijoicm91bGlvNTU1IiwiYSI6ImNsNTB4dmI0bzAwZDQzaW15NGg
 // fonction qui sert à afficher ma carte 
 function MapCity() {
   const [viewport, setViewport] = React.useState({
-    latitude: 47.238022,
-    longitude: 6.024362,
-    zoom: 14,
-    pitch: 40,
-    bearing: 0
+
   });
 // mes différents state
-  const [showPopup, setShowPopup] = React.useState(true);
   const [markers, setMarkers] = React.useState([]);
   const [closeWindow, setCloseWindow] = React.useState(true);
   const [count, setCount] = React.useState(0);
@@ -60,8 +55,8 @@ function MapCity() {
     console.log(list);
   };
 //conserve dans un nouveau tableau les valeurs du dernier marker affiché
-  const markersModify =[ markers[markers.length - 1]];
-  console.log(markersModify, 'tableau modifié');
+  const markersLastElement =[ markers[markers.length - 1]];
+  console.log(markersLastElement, 'tableau modifié');
 
 // soumet le formulaire avec enregistrements des données en localstorage
   const handleSubmit =(event) => {
@@ -69,7 +64,7 @@ function MapCity() {
    localStorage.setItem('textState', text);
    localStorage.setItem('descriptionState', description);
    localStorage.setItem('listState', list);
-   console.log(localStorage.getItem('listState'));
+   localStorage.setItem('markers', markers);
    setCount(count => 0);
    setCloseWindow(window =>false);
   };
@@ -81,6 +76,10 @@ function MapCity() {
       return false;
     } else { return true};
   };
+
+/* useEffect(() => {
+   setMarkers
+  },[]); */
   
   return (
     <>
@@ -96,42 +95,34 @@ function MapCity() {
         {...viewport}
         mapStyle="mapbox://styles/mapbox/streets-v9"
         mapboxAccessToken={MAPBOX_TOKEN}
-        >
-          {showPopup && (
-           <Popup longitude={6.024942} latitude={47.24438}
-            anchor="bottom"
-            onClose={() => setShowPopup(false)}>
-            Vous êtes ici
-           </Popup>)}
-
-          <Marker longitude={6.024942} latitude={47.237038} color="red" />
-          <Marker longitude={6.002154} latitude={47.240724} color="red" />
-          <Marker longitude={6.047449} latitude={47.233789} color="red" />
-          
-           { markers.map((coordinate) => (  
-              <>     
-              <Marker longitude={coordinate.long } latitude={coordinate.lat} color="red" />
-              </>
-            ))}
-            {markersModify.map((coordinate) =>( counter(count) && closeWindow && 
-              <div className="absolute z-10 text-sm bg-gray-200 border-2 border-black w-[220px] h-[270px]" style={{position:'absolute', left:`${coordinate.left +"px"}` ,top:`${coordinate.top+"px"}`}}>
-                <button onClick={handleClickButton} className='ml-[200px]'>X</button>
-                <form className='flex flex-col' onSubmit={handleSubmit}>
-                  <label htmlFor="Nom">Nom</label>
-                      <input type="text" className='mt-[10px] w-[90%] mx-auto' required="required" onChange={handleChangeText}/>
-                  <label htmlFor="Description" className='mt-[10px]'>Description</label>
-                      <textarea id="Description" required="required" onChange={handleChangeDescription} name="Description" className="mt-[10px] w-[90%] mx-auto">
-                      </textarea>
-                  <select name="type" onChange={handleChangeList} required="required" className="mt-[20px] w-[90%] mx-auto h-[25px]">
-                      <option value="">--Choisir une option--</option>
-                      <option value="Entreprise">Entreprise</option>
-                      <option value="Particulier">Particulier</option>
-                      <option value="Collectivité">Collectivité</option>
+        >  
+        <Marker longitude={-113.31} latitude={53.01} color="red" />
+        <Marker longitude={6.002154} latitude={47.240724} color="red" />
+        <Marker longitude={132.047449} latitude={67.233789} color="red" />
+        
+          { markers.map((coordinate) => (  
+            <>     
+            <Marker longitude={coordinate.long } latitude={coordinate.lat} color="red" />
+            </>
+          ))}
+          {markersLastElement.map((coordinate) =>( counter(count) && closeWindow && 
+            <div className="absolute z-10 text-sm bg-gray-200 border-2 border-black w-[220px] h-[270px]" style={{position:'absolute', left:`${coordinate.left +"px"}` ,top:`${coordinate.top+"px"}`}}>
+              <button onClick={handleClickButton} className='ml-[200px]'>X</button>
+              <form className='flex flex-col' onSubmit={handleSubmit}>
+                <label htmlFor="Nom">Nom</label>
+                    <input type="text" className='mt-[10px] w-[90%] mx-auto' required="required" onChange={handleChangeText}/>
+                <label htmlFor="Description" className='mt-[10px]'>Description</label>
+                    <textarea id="Description" onChange={handleChangeDescription} name="Description" className="mt-[10px] w-[90%] mx-auto">
+                    </textarea>
+                <select name="type" onChange={handleChangeList} required="required" className="mt-[20px] w-[90%] mx-auto h-[25px]">
+                    <option value="">--Choisir une option--</option>
+                    <option value="Entreprise">Entreprise</option>
+                    <option value="Particulier">Particulier</option>
+                    <option value="Collectivité">Collectivité</option>
                     </select>
                     <input type="submit" value="Ajouter" className="border-1 border-black bg-blue-600 h-[30px] w-[100px] mx-auto mt-[20px]"/>
                 </form >
               </div>))}
-          <GeolocateControl />
         </Map>
       </div>
     </>
